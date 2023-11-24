9f4c496e9f0d0ff0c3b9fbc2889c861dd145ec6b
CREPIN Maxime <maximecrepin@outlook.com>
2023-11-24 14:38:51 +0100

    first commit

---
+++ scripts/issue-token.js
@@ -0,0 +1,11 @@
+const StakingDapp = artifacts.require('Staking_Dapp')
+
+module.exports = async function(callback){
+
+    let stakingdapp = await StakingDapp.deployed()
+    await stakingdapp.issuedummy()
+
+    console.log("dummy tokens issued!")
+    callback()
+
+}
